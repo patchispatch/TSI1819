d87d96de2ad9f2a147838f1d888eb52de9a4e830
patchispatch <juanov98@gmail.com>
2019-04-17 21:31:10 +0200

    He tenido un día de mierda, no me apetece seguir hoy

--- Prácticas/P1/GVGAI/src/practica_busqueda/Patrick.java
+++ Prácticas/P1/GVGAI/src/practica_busqueda/Patrick.java
@@ -21,6 +21,8 @@ public class Patrick extends BaseAgent {
 
     ArrayList<Node> currentPath;
     Iterator<Node> currentPos;
+    Vector2d nearestGem;
+
     boolean reroute;
 
     // Constructor:
@@ -28,11 +30,11 @@ public class Patrick extends BaseAgent {
         super(so, elapsedTimer);
 
         // Enemigos: murciélagos y escorpiones
-        ArrayList<Integer> enemies = new ArrayList();
-        enemies.add(gs.SCORPION);
-        enemies.add(gs.BAT);
+        ArrayList<Integer> obstacles = new ArrayList();
+        obstacles.add(gs.WALL);
+        obstacles.add(gs.STONE);
 
-        pf = new PathFinder(enemies);
+        pf = new PathFinder(obstacles);
         gs = new GameState(so, this);
         currentPath = new ArrayList();
         reroute = true;
@@ -45,6 +47,7 @@ public class Patrick extends BaseAgent {
         // Actualizar estado del juego:
         gs.update(stateObs);
 
+
         // Realcular ruta si es necesario:
         if(reroute) {
             reroute(stateObs);
@@ -56,25 +59,65 @@ public class Patrick extends BaseAgent {
         // Decidir qué acción tomar según la siguiente posición de la ruta:
         if(currentPos.hasNext()) {
             Node next = currentPos.next();
+
             action = nextMove(next.position);
         }
+        else {
+            System.out.println("Cogiendo gema");
+            action = nextMove(nearestGem);
+            reroute = true;
+        }
 
+        // Para poder apreciar mejor lo tonto que es Patrick:
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         }
-        catch (InterruptedException e) {
-            
-        }
+        catch (InterruptedException e) {}
 
         return action;
     }
 
 
-    // Determina la siguiente acción basándose en la siguiente posición de la ruta
+    // Coger gema:
+    private Types.ACTIONS pickGem(StateObservation stateObs) {
+
+        PlayerObservation player = getPlayer(stateObs);
+        Vector2d playerPosition = gs.playerPosition();
+
+        Types.ACTIONS best = Types.ACTIONS.ACTION_NIL;
+        if (player.getManhattanDistance(nearestGem) == 1) { // Veo si puedo coger esa gema (está a una distancia de 1)
+
+            if (nearestGem.x < playerPosition.x) // gema a la izquierda
+                best = Types.ACTIONS.ACTION_LEFT;
+
+            else if (nearestGem.x > playerPosition.x) // gema a la derecha
+                best = Types.ACTIONS.ACTION_RIGHT;
+
+            else {
+                if (nearestGem.y < playerPosition.y) // gema arriba
+                   best = Types.ACTIONS.ACTION_UP;
+                else
+                    best = Types.ACTIONS.ACTION_DOWN;
+            }
+
+            reroute = true;
+            return best; // Escojo esa acción directamente
+        }
+
+        else {
+            reroute = true;
+            return Types.ACTIONS.ACTION_NIL;
+        }
+    }
+
+
+    // Determina la siguiente acción basándose en la siguiente casilla de la ruta
     private Types.ACTIONS nextMove(Vector2d nextPos){
 
         Types.ACTIONS nextMove;
         Vector2d patrickPos = gs.playerPosition();
+
+        // Comprobamos si es una gema:
 
         if (nextPos.x != patrickPos.x){
             if (nextPos.x > patrickPos.x) {
@@ -107,8 +150,12 @@ public class Patrick extends BaseAgent {
     // Recalcular la ruta a seguir:
     private void reroute(StateObservation stateObs) {
         // Ir a la gema más cercana
+        System.out.println("Recalculando");
+
+        nearestGem = gs.nearestGem();
+
         pf.run(stateObs);
-        currentPath = pf.getPath(gs.playerPosition(), gs.nearestGem());
+        currentPath = pf.getPath(gs.playerPosition(), nearestGem);
 
         for (Node n : currentPath) {
             System.out.println(n.position.x);
